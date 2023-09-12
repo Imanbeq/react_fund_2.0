0a01147a5a23d773bcfb81f916b877f9519813b1
Qlove Iman <imanqlove@gamil.com>
2023-09-12 22:19:53 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import FavouritePages from './pages/FavouritePages';
+import Navigation from './components/Navigation';
+import './index.css'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <Navigation/>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/favourite' element={<FavouritePages/>}/>
+      </Routes>
+    </>
   );
 }
 
